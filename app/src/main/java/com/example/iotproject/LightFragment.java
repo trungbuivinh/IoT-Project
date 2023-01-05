c1fee88e9858021ca925f36d8de61d5bbe30e51e
TrungBui <buivinhtrungqng@gmail.com>
2023-01-05 10:33:55 +0700

    done

---
+++ app/src/main/java/com/example/iotproject/LightFragment.java
@@ -0,0 +1,168 @@
+package com.example.iotproject;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.angads25.toggle.interfaces.OnToggledListener;
+import com.github.angads25.toggle.model.ToggleableView;
+import com.github.angads25.toggle.widget.LabeledSwitch;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LightFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LightFragment extends Fragment {
+    MQTTHelper mqttHelper;
+    LabeledSwitch btnLED1, btnLED2;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LightFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LightFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LightFragment newInstance(String param1, String param2) {
+        LightFragment fragment = new LightFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_light, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLED1 = view.findViewById(R.id.switch1);
+        btnLED2 = view.findViewById(R.id.switch2);
+
+        btnLED1.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                if(isOn){
+                    sendDataMQTT("tung_tran2401/feeds/nutnhan1","1");
+                }else{
+                    sendDataMQTT("tung_tran2401/feeds/nutnhan1","0");
+                }
+            }
+        });
+
+        btnLED2.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                if(isOn){
+                    sendDataMQTT("tung_tran2401/feeds/nutnhan2","1");
+                }else{
+                    sendDataMQTT("tung_tran2401/feeds/nutnhan2","0");
+                }
+            }
+        });
+
+        startMQTT();
+
+    }
+
+
+    public void sendDataMQTT(String topic, String value){
+        MqttMessage msg = new MqttMessage();
+        msg.setId(1234);
+        msg.setQos(0);
+        msg.setRetained(false);
+
+        byte[] b = value.getBytes(Charset.forName("UTF-8"));
+        msg.setPayload(b);
+        try {
+            mqttHelper.mqttAndroidClient.publish(topic, msg);
+        }catch (MqttException e){
+
+        }
+    }
+
+
+    public void startMQTT(){
+        mqttHelper = new MQTTHelper(this.getContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+
+            }
+
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                Log.d("TEST" ,topic+"***"+message.toString());
+
+                if (topic.contains("nutnhan1")){
+                    if(message.toString().equals("1")){
+                        btnLED1.setOn(true);
+                    }else {
+                        btnLED1.setOn(false);
+                    }
+                }else if(topic.contains("nutnhan2")){
+                    if(message.toString().equals("1")){
+                        btnLED2.setOn(true);
+                    }else {
+                        btnLED2.setOn(false);
+                    }
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+    }
+
+
+
+}
